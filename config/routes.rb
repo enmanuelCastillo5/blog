ca6e93ebd59f74a7623389161f142f6d79f97134
enmanuelCastillo <enmanuel.castillo@hotmail.com>
2020-05-10 14:53:24 -0400

    WEEK 4

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'registration/new'
+  get 'registration', to: 'registration#new', as: :registration
+  post 'registration', to: 'registration#create'
   resources :comments
   resources :articles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
