326962c90df4af6a7abd571166b718c60227523a
enmanuelCastillo <enmanuel.castillo@hotmail.com>
2020-05-25 19:54:26 -0400

    WEEK 6 GEMS

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("trix")
+require("@rails/actiontext")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -15,3 +17,15 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import "bootstrap";
+import "@fortawesome/fontawesome-free/js/all";
+import "../stylesheets/application";
+
+document.addEventListener("turbolinks:load", () => {
+  $('[data-toggle="tooltip"]').tooltip()
+  $('[data-toggle="popover"]').popover()
+})
+
+require("trix")
+require("@rails/actiontext")
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,7 @@
 class Article < ApplicationRecord
-  has_many :comments
-  validates :title, :content, presence: true
+  validates :title, presence: true
   validates :title, uniqueness: true
+  has_rich_text :content
+  has_many :comments
   belongs_to :user
 end
